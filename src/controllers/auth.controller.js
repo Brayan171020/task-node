f73b4b104bfc4cd1a3077eafaa59a9a9c9953095
Brayan171020 <brayangt1710@gmail.com>
2023-10-25 17:18:34 -0400

    add Token

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,14 +1,29 @@
 import User from '../models/user.model.js';
+import bcrypt  from 'bcryptjs';
+import { createAccessToken } from '../libs/jwt.js';
 
 export const register = async (req,res) => {
     const {email, password, username} = req.body;
 
     try {
-        const newUser = new User({email: email, password: password, username: username});
+        const passwordHash = await bcrypt.hash(password, 10);
+
+        const newUser = new User({email: email, password: passwordHash, username: username});
+
         const userSaved = await newUser.save();
-        res.json(userSaved);
+
+        const token = await createAccessToken({id: userSaved._id});
+
+        res.cookie('token',  token);
+        res.json({
+            id: userSaved._id,
+            username: userSaved.username,
+            email: userSaved.email,
+            createdAt: userSaved.createdAt,
+            updateAt: userSaved.updateAt
+        });
     } catch (error) {
-        console.log(error);
+        res.status(500).json({message: error.message});
     }
     
 }
