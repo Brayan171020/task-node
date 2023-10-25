9edfd419baae2c56610de874b8f98b61572d0cc7
Brayan171020 <brayangt1710@gmail.com>
2023-10-25 17:59:12 -0400

    add login endpoint

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import  {login}  from '../controllers/auth.controller.js';
 import  {register}  from '../controllers/auth.controller.js';
+import  {logout}  from '../controllers/auth.controller.js';
 
 const router = Router()
 
 router.post('/register', register)
 router.post('/login', login)
+router.post('/logout', logout)
 
 export default router
