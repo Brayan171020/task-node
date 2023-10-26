b1f1723960ddfb0df3b88cf99c92777acb717c16
Brayan171020 <brayangt1710@gmail.com>
2023-10-25 23:47:05 -0400

    add authSchema and tasksSchema

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { authRequired } from "../middlewares/validateToken.js";
 import { getTasks, createTask, getTask, updateTask, delteTask } from "../controllers/tasks.controller.js";
+import { validateSchema } from "../middlewares/validator.middlewares.js";
+import { createTaskSchema } from "../schemas/tasks.schema.js";
 
 const router = Router();
 
@@ -8,7 +10,7 @@ router.get('/tasks', authRequired, getTasks)
 
 router.get('/tasks/:id', authRequired, getTask)
 
-router.post('/tasks', authRequired, createTask)
+router.post('/tasks', authRequired, validateSchema(createTaskSchema), createTask)
 
 router.delete('/tasks/:id', authRequired, delteTask)
 
